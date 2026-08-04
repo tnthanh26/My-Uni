c8c4bc9443ab61173663d97199d76471f75b90b8
Henry Huynh <127390090+Hua1704@users.noreply.github.com>
2026-08-04 15:12:32 +0700

    Fix theme mode caching bug on HomePage by rebuilding main home content on theme change

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -91,7 +91,6 @@ class HomePageState extends State<HomePage> {
   bool _hasShownWeatherAlert = false;
 
   StreamSubscription<DocumentSnapshot>? _userDocSubscription;
-  Widget? _homeMainPage;
 
   @override
   void initState() {
@@ -1319,10 +1318,8 @@ class HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    _homeMainPage ??= _buildMainHomeContent();
-
     final List<Widget> pages = [
-      _homeMainPage!,
+      _buildMainHomeContent(),
       const EventPage(),
       const ChatbotPage(),
       MySpaceScreen(isActive: _selectedIndex == 3),
